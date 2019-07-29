16eeda591779d9262ca9c22600c707c5f7e2b60b
zhangailing <1581564405@qq.com>
2019-07-29 11:22:17 +0800

    '修改配置‘

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import './index.styl';
 export default class App extends Component {
 	render() {
-		return <div>App</div>;
+		return (
+			<div className="test">
+				App
+				<div className="myColor">mycolor</div>
+			</div>
+		);
 	}
 }
